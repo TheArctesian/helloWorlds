25a5e136c9d1e2f6b9fdfbcc0290c7736e6e4fd9
theArctesian <stephen.d.okita@gmail.com>
2022-05-25 10:03:58 +0800

    julia

--- HelloWorlds/helloworld.jl
+++ HelloWorlds/helloworld.jl
@@ -1,2 +1,8 @@
+# Ok I really like this language 
+# And I really hate java 
+# So I am going to type some shit here
+# so the this is fatter then the java file
+# so on github the repo shows julia 
+# instead of that disgusting language
 println("hello world")
 
